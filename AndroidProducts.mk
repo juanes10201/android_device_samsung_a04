e4fce1e8a41a9928dfe9d51a034d71d7df9d494a
juanes <juanesteban10201@gmail.com>
2024-07-19 15:14:56 -0300

    Samsung a04 device tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_a04.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_a04-user \
+    lineage_a04-userdebug \
+    lineage_a04-eng
